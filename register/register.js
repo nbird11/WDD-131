f13a67c88b30d41b37d9d4da8c86dfe12121ad8e
Nathan Bird <nathanbirdka@gmail.com>
2024-11-09 22:01:56 -0700

    Registration Form complete

--- register/register.js
+++ register/register.js
@@ -9,20 +9,20 @@ function getParticipantTemplate(num) {
   return `<section class="participant${num}">
           <p>Participant ${num}</p>
           <div class="item">
-            <label for="fname"> First Name<span>*</span></label>
-            <input id="fname" type="text" name="fname" value="" required />
+            <label for="fname${num}"> First Name<span>*</span></label>
+            <input id="fname${num}" type="text" name="fname" value="" required />
           </div>
           <div class="item activities">
-            <label for="activity">Activity #<span>*</span></label>
-            <input id="activity" type="text" name="activity" />
+            <label for="activity${num}">Activity #<span>*</span></label>
+            <input id=activity${num} type="text" name="activity" />
           </div>
           <div class="item">
-            <label for="fee">Fee ($)<span>*</span></label>
-            <input id="fee" type="number" name="fee" />
+            <label for="fee${num}">Fee ($)<span>*</span></label>
+            <input id="fee${num}" type="number" name="fee" />
           </div>
           <div class="item">
-            <label for="date">Desired Date <span>*</span></label>
-            <input id="date" type="date" name="date" />
+            <label for="date${num}">Desired Date <span>*</span></label>
+            <input id="date${num}" type="date" name="date" />
           </div>
           <div class="item">
             <p>Grade</p>
@@ -47,4 +47,25 @@ function getParticipantTemplate(num) {
 
 document.getElementById('add').addEventListener('click', (e) => {
   e.target.insertAdjacentHTML('beforebegin', getParticipantTemplate(++numParticipants));
+});
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  /** @type {HTMLInputElement[]} */
+  let feeElements = document.querySelectorAll("[id^=fee]");
+  feeElements = [...feeElements]
+  const fees = feeElements.reduce((acc, curr) => {
+    return acc + parseFloat(curr.value);
+  }, 0);
+
+  const adultName = document.getElementById('adult_name').value;
+
+  /** @type {HTMLFormElement} */
+  const form = document.querySelector('form');
+  form.style.display = 'none';
+  document.getElementById('summary').innerHTML = `
+    <h2>Thank you, ${adultName} for registering.</h2>
+    <p>You have registered ${numParticipants} participants and owe $${fees} in fees.</p>
+  `;
 });
