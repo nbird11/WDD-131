9d67284796c9d118cc108125e1f19fab00707ebe
Nathan Bird <nathanbirdka@gmail.com>
2024-11-30 00:49:52 -0700

    Personal Website site plan

---
+++ resume/README.md
@@ -0,0 +1,8 @@
+# Dynamic Resume
+This is a dynamic resume website that takes data from my LinkedIn profile to automatically update my web resume. It provides a clean, professional presentation of my work experience, skills, and education that stays in sync with my LinkedIn profile.
+
+## Planned Features
+- Ability to automatically update mjs data when LikedIn profile changes
+- Professional styling and layout
+- Mobile-responsive design
+- Easy maintenance through LinkedIn as single source of truth
---
+++ resume/profile.mjs
@@ -0,0 +1,116 @@
+export default profile = {
+  name: "Nathan Bird",
+  contact: {
+    location: "Rexburg, ID",
+    email: "nathanbirdka+resume@gmail.com",
+    phone: "(385) 392-5995"
+  },
+  bio: "Former Backend Software Development Intern at FamilySearch || Software Engineering Student at Brigham Young University-Idaho",
+  websites: [
+    { name: "GitHub", url: "https://github.com/nbird11" },
+    { name: "LinkedIn", url: "https://www.linkedin.com/in/nathanabird/" },
+    { name: "Personal Webserver", url: "https://nbird.dev/" }
+  ],
+  about: "I'm currently a full-time student at BYU-Idaho studying Software Engineering with a focus on Software Design, optimizing algorithms, and reducing complexity. Over the summer, I interned as a Backend Web Developer at FamilySearch, where I built and maintained backend REST APIs, handled data collection, and monitored AWS builds/deploys. Notably, I contributed to a Paris 2024 Olympics project under a tight deadline.\nMy passion for technology extends beyond my professional life. I enjoy working on personal programming projects, which allows me to explore new technologies and enhance my skills. I am also an active member of the Association for Computing Machinery (ACM) at BYU-Idaho, participating in discussions on various Computer Science topics. Outside of tech, I love playing board games with friends, practicing the piano, and singing.\nDuring the height of the covid pandemic, I served a proselyting mission for the Church of Jesus Christ of Latter-day Saints where I taught, performed service, and provided spiritual aid in Colorado and Brazil, in English and Portuguese respectively.\nLooking forward to contributing to innovative projects and making a meaningful impact in the tech community.",
+  education: [
+    {
+      logo: "https://media.licdn.com/dms/image/v2/D560BAQHxHHN9dJ9TnA/company-logo_100_100/company-logo_100_100/0/1726260984458?e=1741219200&v=beta&t=z4722-mh-n4kyFSqWmUXEoLTox9dWQ3kBHaAIp50glg",
+      institution: "Brigham Young University-Idaho",
+      degree: "Bachelor of Science in Software Engineering",
+      start: "2019",
+      end: "2025",
+      gpa: "3.98",
+      activities: [
+        "Association for Computing Machinery (ACM)",
+      ]
+    },
+    {
+      logo: "https://media.licdn.com/dms/image/v2/C4E0BAQF-Aqi67LbmYg/company-logo_100_100/company-logo_100_100/0/1630592259166?e=1741219200&v=beta&t=StTWnvtlf1difjqf79z60TqWlJbAI3tH4OALkylbwLI",
+      institution: "Santa Teresa High School",
+      degree: "High School Diploma",
+      start: "2015",
+      end: "2019",
+      activities: [
+        "Computer Science Club",
+        "Marching Band",
+      ]
+    }
+  ],
+  experience: [
+    {
+      logo: "https://media.licdn.com/dms/image/v2/D560BAQHxHHN9dJ9TnA/company-logo_100_100/company-logo_100_100/0/1726260984458?e=1741219200&v=beta&t=z4722-mh-n4kyFSqWmUXEoLTox9dWQ3kBHaAIp50glg",
+      title: "Programming Tutor",
+      company: "Brigham Young University–Idaho",
+      employmentType: "Part-time",
+      start: "Sep 2024",
+      end: "Present",
+      location: "Rexburg, Idaho, United States",
+      locationType: "On-site",
+      bullets: [
+        "Lead individual and group sessions to assess understanding of software design principles in Python and C++, as well as low-level language skills in C.",
+        "Tutor students in Algorithm Design, Encapsulation Design, and C language classes.",
+        "Guide students in discovering solutions to course-related questions independently.",
+      ]
+    },
+    {
+      logo: "https://media.licdn.com/dms/image/v2/C4E0BAQGoC_lEdSbT4w/company-logo_100_100/company-logo_100_100/0/1645790791911/familysearch_logo?e=1741219200&v=beta&t=z84KG9Du3is-2JhbF_mwTM35lx9vqZqs6UI5_j3EhQA",
+      title: "Backend Software Development Intern",
+      company: "FamilySearch",
+      employmentType: "Internship",
+      start: "Apr 2024",
+      end: "Aug 2024",
+      location: "Lehi, Utah, United States",
+      locationType: "Hybrid",
+      bullets: [
+        "Developed and maintained 7 backend REST APIs and microservices in Java Spring Boot.",
+        "Consulted on the architectural discussion of 3 backend APIs for new projects.",
+        "Delivered a prototype demonstration of a current-events user experience developed under a tight deadline.",
+        "Implemented over 1000 unit and integration tests utilizing Spring methodologies.",
+      ]
+    },
+    {
+      logo: "https://media.licdn.com/dms/image/v2/D560BAQHxHHN9dJ9TnA/company-logo_100_100/company-logo_100_100/0/1726260984458?e=1741219200&v=beta&t=z4722-mh-n4kyFSqWmUXEoLTox9dWQ3kBHaAIp50glg",
+      title: "Research Assistant",
+      company: "Brigham Young University–Idaho",
+      employmentType: "Part-time",
+      start: "Dec 2023",
+      end: "May 2024",
+      location: "Rexburg, Idaho, United States",
+      locationType: "Remote",
+      bullets: [
+        "Conducted in-depth analysis of Angular Ionic framework as part of a comparative study across 5 mobile app platforms.",
+        "Developed a mobile app prototype using Angular Ionic, evaluating project setup, learning curve, and maintenance challenges.",
+        "Integrated AWS Amplify and transitioned to Google Firestore, achieving a 30% increase in development efficiency, and assessed other pitfalls specific to Angular development.",
+        "Documented findings across 15 key metrics, including resource availability and development efficiency, contributing to a comprehensive cross-platform comparison report.",
+      ]
+    },
+    {
+      logo: "https://media.licdn.com/dms/image/v2/D560BAQHxHHN9dJ9TnA/company-logo_100_100/company-logo_100_100/0/1726260984458?e=1741219200&v=beta&t=z4722-mh-n4kyFSqWmUXEoLTox9dWQ3kBHaAIp50glg",
+      title: "Programming Tutor",
+      company: "Brigham Young University–Idaho",
+      employmentType: "Seasonal",
+      start: "Oct 2023",
+      end: "Dec 2023",
+      location: "Rexburg, Idaho, United States",
+      locationType: "On-site",
+      bullets: [
+        "Led individual and group sessions with over 30 students to assess their understanding of Python.",
+        "Tutored students in Intro to Programming and Algorithm Design classes.",
+        "Guided students in discovering solutions to course-related questions independently.",
+      ]
+    },
+    {
+      logo: null,
+      title: "Software Consultant",
+      company: "Self-Employed",
+      employmentType: "Freelance",
+      start: "Jan 2022",
+      end: "Sep 2023",
+      location: "Saratoga Springs, Utah, United States",
+      locationType: "Remote",
+      bullets: [
+        "Built and developed custom software for clients looking for professional and well-maintained solutions.",
+      ]
+    }
+  ]
+};
